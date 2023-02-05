627a33868227c4c8937a428b91bc5f59684d0263
Megnsyue Amao Tsai <mengsyue.tsai@outlook.com>
2023-02-05 11:53:52 +0800

    Websocket event listeners

--- market-data-engine-client/index.ts
+++ market-data-engine-client/index.ts
@@ -1,7 +1,35 @@
+import { RawData, WebSocket } from "ws";
 
 
 class MarketDataEngineClient {
+    private websocketClient?: WebSocket;
 
+    constructor() {
+    }
+
+    public connect = () => {
+        const port = 8080;
+        const url = `ws://localhost:${port}`;
+        this.websocketClient = new WebSocket(url);
+        this.websocketClient
+            .on('open', this.onWebSocketOpened)
+            .on('close', this.onWebSocketClosed)
+            .on('error', this.onWebSocketError)
+            .on('message', this.onWebSocketMessageReceived)
+            ;
+    }
+
+    private onWebSocketOpened = () => {
+    }
+
+    private onWebSocketClosed = () => {
+    }
+
+    private onWebSocketError = (error: Error) => {
+    }
+
+    private onWebSocketMessageReceived = (data: RawData) => {
+    }
 }
 
 export default MarketDataEngineClient;
