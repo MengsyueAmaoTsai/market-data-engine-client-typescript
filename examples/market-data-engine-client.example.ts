5cbfb37e6344a1adcbcb0bb708ea09d377ecf60e
Megnsyue Amao Tsai <mengsyue.tsai@outlook.com>
2023-03-12 18:09:47 +0800

    format code

--- examples/market-data-engine-client.example.ts
+++ examples/market-data-engine-client.example.ts
@@ -1,15 +1,13 @@
-
-
-import MarketDataEngineClient from "../market-data-engine-client";
+import MarketDataEngineClient from '../market-data-engine-client';
 
 const symbols = ['TX-1', 'NQ-1'];
 
 const client = new MarketDataEngineClient();
 
 client.on('connected', () => {
-    symbols.forEach(symbol => {
-        client.subscribe(symbol);
-    });
+  symbols.forEach(symbol => {
+    client.subscribe(symbol);
+  });
 });
 
 client.connect();
--- market-data-engine-client/index.ts
+++ market-data-engine-client/index.ts
@@ -1,63 +1,57 @@
-import EventEmitter from "events";
-import { RawData, WebSocket } from "ws";
+import EventEmitter from 'events';
+import { RawData, WebSocket } from 'ws';
 
 interface MarketDataEngineClient {
-    on(event: 'connected', listener: () => void): this;
+  on(event: 'connected', listener: () => void): this;
 }
 
 class MarketDataEngineClient extends EventEmitter {
-    private websocketClient?: WebSocket;
+  private websocketClient?: WebSocket;
 
-    constructor() {
-        super()
-    }
+  constructor() {
+    super();
+  }
 
-    public connect = (): void => {
-        const port = 8080;
-        const url = `ws://localhost:${port}`;
-        this.websocketClient = new WebSocket(url);
-        this.websocketClient
-            .on('open', this.onWebSocketOpened)
-            .on('close', this.onWebSocketClosed)
-            .on('error', this.onWebSocketError)
-            .on('message', this.onWebSocketMessageReceived)
-            ;
-    }
+  public connect = (): void => {
+    const port = 8080;
+    const url = `ws://localhost:${port}`;
+    this.websocketClient = new WebSocket(url);
+    this.websocketClient
+      .on('open', this.onWebSocketOpened)
+      .on('close', this.onWebSocketClosed)
+      .on('error', this.onWebSocketError)
+      .on('message', this.onWebSocketMessageReceived);
+  };
 
-    public disconnect = (): void => {
-    }
+  public disconnect = (): void => {};
 
-    public subscribe = (symbol: string): void => {
-        const request = {
-            clientId: 'market-data-engine-client-1',
-            action: 'subscribe',
-            symbol: symbol,
-        }
-        this.websocketClient?.send(JSON.stringify(request));
-    }
+  public subscribe = (symbol: string): void => {
+    const request = {
+      clientId: 'market-data-engine-client-1',
+      action: 'subscribe',
+      symbol: symbol,
+    };
+    this.websocketClient?.send(JSON.stringify(request));
+  };
 
-    public unsubscribe = (symbol: string): void => {
-        const request = {
-            clientId: 'market-data-engine-client-1',
-            action: 'unsubscribe',
-            symbol: symbol 
-        }
-        this.websocketClient?.send(JSON.stringify(request));
-    }
+  public unsubscribe = (symbol: string): void => {
+    const request = {
+      clientId: 'market-data-engine-client-1',
+      action: 'unsubscribe',
+      symbol: symbol,
+    };
+    this.websocketClient?.send(JSON.stringify(request));
+  };
 
-    private onWebSocketOpened = (): void => {
-        this.emit('connected');
-    }
+  private onWebSocketOpened = (): void => {
+    this.emit('connected');
+  };
 
-    private onWebSocketClosed = (): void => {
-    }
+  private onWebSocketClosed = (): void => {};
 
-    private onWebSocketError = (error: Error): void => {
-    }
+  private onWebSocketError = (error: Error): void => {};
 
-    private onWebSocketMessageReceived = (data: RawData): void => {
-    }
+  private onWebSocketMessageReceived = (data: RawData): void => {};
 }
-
 
 export default MarketDataEngineClient;
